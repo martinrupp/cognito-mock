5ecdc0a844c4eb26b998d1f1c8865b19785096b8
Martin Rupp <martin.rupp@everest-erp.com>
2021-12-02 12:42:08 +0100

    feat: adding filters for listUsers

--- src/targets/listUsers.ts
+++ src/targets/listUsers.ts
@@ -1,5 +1,5 @@
-import { Services } from "../services";
-import { UserAttribute } from "../services/userPoolClient";
+import { Services } from '../services';
+import { UserAttribute } from '../services/userPoolClient';
 
 interface Input {
   UserPoolId: string;
@@ -14,7 +14,7 @@ export interface DynamoDBUserRecord {
   UserCreateDate: number;
   UserLastModifiedDate: number;
   Enabled: boolean;
-  UserStatus: "CONFIRMED" | "UNCONFIRMED" | "RESET_REQUIRED";
+  UserStatus: 'CONFIRMED' | 'UNCONFIRMED' | 'RESET_REQUIRED';
   Attributes: readonly UserAttribute[];
 }
 
@@ -25,11 +25,42 @@ interface Output {
 
 export type ListUsersTarget = (body: Input) => Promise<Output>;
 
-export const ListUsers = ({
-  cognitoClient,
-}: Services): ListUsersTarget => async (body) => {
+// see https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_ListUsers.html
+export const ListUsers = ({ cognitoClient }: Services): ListUsersTarget => async (body) => {
   const userPool = await cognitoClient.getUserPool(body.UserPoolId);
-  const users = await userPool.listUsers();
+  let users = await userPool.listUsers();
+
+  if (body?.Filter) {
+    // e.g. 'sub="fa34-a84d"'
+    const regex = /([a-z_:]*)[ ]*(\^?=)[ ]*"(.*)"/;
+    const res = regex.exec(body.Filter);
+    if (res) {
+      const supportedFilters = [
+        'username',
+        'email',
+        'phone_number',
+        'name',
+        'given_name',
+        'family_name',
+        'preferred_username',
+        'cognito:user_status',
+        'status',
+        'sub',
+      ];
+
+      const [, field, eqType, value] = res;
+      if (!supportedFilters.includes(field)) {
+        throw new Error(`unsupported filter ${field}`);
+      }
+      // using '=' is equals, ^= is starts with, e.g. 'given_name ^= "Ma"' is first name starting with Ma
+      const matchEq = (attr: string) => attr == value;
+      const matchStartsWith = (attr: string) => attr.startsWith(value);
+      const match = eqType === '^=' ? matchStartsWith : matchEq;
+      users = users.filter((e) => e.Attributes.find((e2) => e2.Name == field && match(e2.Value)));
+    } else {
+      throw new Error(`could not parse filter ${body.Filter}`);
+    }
+  }
 
   return {
     Users: users.map((user) => ({
